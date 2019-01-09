0bc30b985d8c7f899f022b140898226c7f75429b
dannykrug <dannykrug@gmail.com>
2019-01-09 10:23:57 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//Index.ios.js - place code in here for iOS!!
+
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <Text>Welcome to Aprilgram</Text>
+  );
+};
+
+AppRegistry.registerComponent('aprilgram', () => App);
