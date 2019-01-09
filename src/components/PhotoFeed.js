2acff95390394fdf6861c3cdbfe2443c83542fc7
dannykrug <dannykrug@gmail.com>
2019-01-09 11:34:55 -0500

    adding photos

--- src/components/PhotoFeed.js
+++ src/components/PhotoFeed.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, Image, View } from 'react-native';
 
 class PhotoFeed extends Component {
   render() {
     return (
-      <Text>Photos Go Here</Text>
+      <View>
+        <Text>Photos Go Here</Text>
+        <Image
+        style={styles.imageStyle}
+        source={{ uri: 'http://www.cutestpaw.com/wp-content/uploads/2016/05/POOF-puppy.jpeg' }}
+        />
+      </View>
     );
   }
 }
+
+const styles = {
+  imageStyle: {
+    height: 350,
+    width: 350
+  }
+};
 
 export default PhotoFeed;
