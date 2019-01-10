7788dacd976a41d38ae1d467264e86b7b19f7887
dannykrug <dannykrug@gmail.com>
2019-01-10 09:53:43 -0500

    starting on likes

--- src/components/PhotoDetail.js
+++ src/components/PhotoDetail.js
@@ -1,31 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, Image, View } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
-
-const PhotoDetail = ({ photo }) => {
-  console.log(photo.title);
-  return (
-    <Card>
-      <CardSection>
-        <Text>{photo.title}</Text>
-      </CardSection>
-
-      <CardSection>
-        <Image style={styles.imageStyle} source={{ uri: photo.img }} />
-      </CardSection>
-
-      <CardSection>
-        <View style={styles.likesCaptionContentStyle}>
-          <Button>Like</Button>
-          <Text>{photo.likes} likes</Text>
-          <Text>{photo.caption}</Text>
-        </View>
-      </CardSection>
-    </Card>
-  );
-};
 
 const styles = {
   likesCaptionContentStyle: {
@@ -52,5 +29,39 @@ const styles = {
     width: null
   }
 };
+
+class PhotoDetail extends Component {
+
+  render() {
+    let likes = this.props.photo.likes;
+
+    const addLike = () => {
+      likes += 1;
+      console.log(likes);
+    };
+
+    console.log(this.props);
+  return (
+    <Card>
+      <CardSection>
+        <Text>{this.props.photo.title}</Text>
+      </CardSection>
+
+      <CardSection>
+        <Image style={styles.imageStyle} source={{ uri: this.props.photo.img }} />
+      </CardSection>
+
+      <CardSection>
+        <View style={styles.likesCaptionContentStyle}>
+          <Button onPress={addLike}>Like</Button>
+          <Text>{likes} likes</Text>
+          <Text>{this.props.photo.caption}</Text>
+        </View>
+      </CardSection>
+    </Card>
+  );
+}
+
+}
 
 export default PhotoDetail;
